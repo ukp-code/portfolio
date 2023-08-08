a600c7eb8a901e89aad46e31ca5fe2a5d07b8489
Lvinfosolution <109540617+ukp-code@users.noreply.github.com>
2023-08-08 23:51:03 +0530

    Update index.js

--- pages/about/index.js
+++ pages/about/index.js
@@ -78,7 +78,7 @@ const aboutData = [
 ];
 
 const About = () => {
-  const [index,setIndex]=useState(0)
+  const [index, setIndex] = useState(0)
   return <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
     <Circles />
     <motion.div variants={fadeIn("right", 0.2)} initial="hidden" animate="show" exit='hidden' className="hidden xl:flex absolute bottom-0 -left-[370px]">
@@ -88,9 +88,9 @@ const About = () => {
       <div>text</div>
       <div>
         <div className="flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4">
-          {aboutData.map((item,i)=>{
+          {aboutData.map((item, i) => {
             return (
-              <div key={i} className={`${i==index && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300' } cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0 z-10`} onClick={()=>setIndex(i)}>{item.title}</div>
+              <div key={i} className={`${i == index && 'text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300'} cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0 z-10`} onClick={() => setIndex(i)}>{item.title}</div>
             )
           })}
         </div>
